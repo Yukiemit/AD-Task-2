24c3ee39f53a82fa050ffcb3a7ab635142000f9f
yukiemit <202310242@fit.edu.ph>
2025-05-20 00:57:40 +0800

    fix (saturday page) added functions and fragmentation

--- page6/saturday.php
+++ page6/saturday.php
@@ -4,32 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Saturday</title>
-    <link rel="stylesheet" href="/AD-TASK1/page6/assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-    <div class="container">
-        <h1>Saturday Schedule</h1>
+<?php
+function getSchedule($day) {
+    $schedules = [
+        "Saturday" => [
+            "7:00 AM - 9:00 AM | Workout",
+            "11:00 AM - 12:00 PM | Lunch",
+            "1:00 PM - 3:50 PM | Coding",
+            "4:00 PM - 6:00 PM | School works",
+            "6:00 PM - ONWARDS | Rest time"
+        ]
+    ];
+    return $schedules[$day] ?? [];
+}
+
+function renderSchedule($schedule) {
+    $html = '';
+    foreach ($schedule as $item) {
+        $html .= "<p>$item</p>";
+    }
+    return $html;
+}
+
+$day = "Saturday";
+$schedule = getSchedule($day);
+?>
+<div class="container">
+        <h1><?php echo $day; ?> Schedule</h1>
         <div class="schedule">
-            <?php
-            $day = "Saturday";
-            $schedule = [];
-
-            if ($day == "Saturday") {
-                $schedule = [
-                "7:00 AM - 9:00 AM | Workout",
-                "11:00 AM - 12:00 PM | Lunch",
-                "1:00 PM - 3:50 PM | Coding",
-                "4:00 PM - 6:00 PM | School works",
-                "6:00 PM - ONWARDS | Rest time",
-                ];
-            }
-
-        for ($i = 0; $i < count($schedule); $i++) {
-            echo "<p>" . $schedule[$i] . '</p>';
-        }
-            ?>
+            <?php echo renderSchedule($schedule); ?>
         </div>
-        <a class="day-link" href="/AD-TASK1/index.php">Back to Home</a>
+        <a class="day-link" href="../index.php">Back to Home</a>
     </div>
 </body>
 </html>
