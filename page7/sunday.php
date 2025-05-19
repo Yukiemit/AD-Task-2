937fa54fc3d6d66634b4e9e332ec36f73f23d5e9
yukiemit <202310242@fit.edu.ph>
2025-05-20 01:22:37 +0800

    fix (sunday page) fragmented the code and added the following requirements

--- page7/sunday.php
+++ page7/sunday.php
@@ -4,33 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sunday</title>
-    <link rel="stylesheet" href="/AD-TASK1/page7/assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
+<?php
+function getSchedule($day) {
+    $schedules = [
+        "Sunday" => [
+            "7:00 AM - 9:00 AM | Workout",
+            "11:00 AM - 12:00 PM | Lunch",
+            "1:00 PM - 3:50 PM | Coding",
+            "4:00 PM - 5:00 PM | School works",
+            "6:00 PM - 7:00 PM | Church",
+            "8:00 PM - ONWARDS | Rest Time"
+        ]
+    ];
+    return $schedules[$day] ?? [];
+}
+
+function renderSchedule($schedule) {
+    $html = '';
+    foreach ($schedule as $item) {
+        $html .= "<p>$item</p>";
+    }
+    return $html;
+}
+
+$day = "Sunday";
+$schedule = getSchedule($day);
+?>
+
     <div class="container">
-        <h1>Sunday Schedule</h1>
+        <h1><?php echo $day; ?> Schedule</h1>
         <div class="schedule">
-            <?php
-            $day = "Sunday";
-            $schedule = [];
-
-            if ($day == "Sunday") {
-                $schedule = [
-                "7:00 AM - 9:00 AM | Workout",
-                "11:00 AM - 12:00 PM | Lunch",
-                "1:00 PM - 3:50 PM | Coding",
-                "4:00 PM - 5:00 PM | School works",
-                "6:00 PM - 7:00 | Church",
-                "8:00 PM - ONWARDS | Rest Time",
-                ];
-            }
-
-        for ($i = 0; $i < count($schedule); $i++) {
-            echo "<p>" . $schedule[$i] . '</p>';
-        }
-            ?>
+            <?php echo renderSchedule($schedule); ?>
         </div>
-        <a class="day-link" href="/AD-TASK1/index.php">Back to Home</a>
+        <a class="day-link" href="../index.php">Back to Home</a>
     </div>
 </body>
 </html>
+
+
+
+
+
+
+
